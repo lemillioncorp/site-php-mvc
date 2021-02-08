4e25ec78b90590dc312f788a24736bd0c35fe380
Akreman <antoniotavaresuchihakozan@gmail.com>
2021-02-08 23:14:06 +0100

    Adicionando Novos Arquivos de Configuração e Instalando dependencias(Twig) no composer.json

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # site-php-mvc
+
+# Ferramentas
+VSCODE - Editor de Texto
+AMPP - Servidor Web local
+MySQL - Sistema de Gerenciamento de Banco de Dados
+Microsoft Edge - Navegador.
+
+
+# Toda a Base do Projecto foi criado
---
+++ index.php
@@ -0,0 +1,52 @@
+<?php
+
+//REQUISIÇÃO OU IMPORTACÃO
+require_once 'app/Core/Core.php';
+
+require_once 'lib/Database/Connection.php';
+
+require_once 'app/Controller/HomeController.php';
+require_once 'app/Controller/ErroController.php';
+require_once 'app/Controller/SobreController.php';
+
+
+require_once 'app/Model/Postagem.php';
+
+require_once 'vendor/autoload.php';
+//FIM DA IMPORTAÇÃO
+
+
+$template = file_get_contents('app/Template/estrutura.html');
+
+ob_start();
+    $core = new Core;
+    $core->start($_GET);
+
+    $saida = ob_get_contents();
+        ob_clean();
+
+        $templatePronto = str_replace('{{area_dinamica}}', $saida, $template);
+
+echo $templatePronto;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
