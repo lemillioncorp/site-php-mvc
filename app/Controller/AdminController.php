12a64b2c53e9c8161c050582669718a2b5611c2a
Stain António Kozan AK <70895080+lemillioncorp@users.noreply.github.com>
2021-03-01 21:00:26 +0000

    Criando o Crud - Update
    Atualizando as Publicações do Site de forma dinamica

--- app/Controller/AdminController.php
+++ app/Controller/AdminController.php
@@ -4,14 +4,17 @@ class AdminController
 {
    
 
-    public function index(){
+    public function index()
+    {
          //echo "PÁGINA HOME"; 
             $loader = new \Twig\Loader\FilesystemLoader('app/View');
             $twig = new \Twig\Environment($loader); 
-
             $template = $twig->load('admin.html');
 
+            $objPostagens = Postagem::SelecionaTodos();
+
             $parametros = array();
+            $parametros['postagens'] = $objPostagens;
             
            $conteudo = $template->render($parametros);
             echo $conteudo;
@@ -31,8 +34,57 @@ class AdminController
             echo $conteudo;
     }
 
-    public function insert(){
-        Postagem::insert($_POST);
+    public function insert()
+    {
+        try {
+             Postagem::insert($_POST);
+            echo '<script>alert("Publicado com Sucesso!");</script>';
+             echo '<script>location.href="http://localhost/git-servidor-php/criando-site-mvc/?pagina=admin&metodo=index"</script>';
+        } catch (Exception $e) {
+          
+               echo '<script>location.href="http://localhost/git-servidor-php/criando-site-mvc/?pagina=admin&metodo=create"</script>';
+
+                echo '<script>alert(" '.$e->getMessage().'"Erro ao Cadastar Postagem");</script>';
+        }
+    }
+
+    public function change($id)
+    {
+
+          //echo "PÁGINA HOME"; 
+            $loader = new \Twig\Loader\FilesystemLoader('app/View');
+            $twig = new \Twig\Environment($loader); 
+
+            $template = $twig->load('update.html');
+
+            $post = Postagem::SelecionaPostPorId($id);
+
+            $parametros = array();
+            $parametros['id'] = $post->id;
+            $parametros['titulo'] = $post->titulo; 
+            $parametros['conteudo'] = $post->conteudo;
+            
+           $conteudo = $template->render($parametros);
+            echo $conteudo;
+    }
+    public function update()
+    {
+        try {
+              Postagem::update($_POST);
+               echo '<script>alert("Publicação Alterada com Sucesso!");</script>';
+
+             echo '<script>location.href="http://localhost/git-servidor-php/criando-site-mvc/?pagina=admin&metodo=index"</script>';
+        } catch (Exception $e) {
+          
+               echo '<script>location.href="http://localhost/git-servidor-php/criando-site-mvc/?pagina=admin&metodo=index&metodo=change&id=$_POST['.$_POST['id'].']"</script>';
+
+                echo '<script>alert(" '.$e->getMessage().'");</script>';
+        }
+      
+    }
+    public function delete()
+    {
+        
     }
 
      
--- app/Model/Postagem.php
+++ app/Model/Postagem.php
@@ -49,7 +49,66 @@
         }
         public static function insert($dadosPost)
         {
+            if (empty($dadosPost['titulo']) || empty($dadosPost['conteudo']) ) {
+               throw new Exception("Preencha todos os campos");
+
+               return false;
+            }
             
+              $con = Connection::getConn();
+
+              $query = "INSERT INTO postagem (titulo, conteudo) VALUES(:tit, :cont)";
+              $sql = $con->prepare($query);
+              $sql->bindValue(":tit", $dadosPost['titulo']);
+            $sql->bindValue(":cont", $dadosPost['conteudo']);
+             $res = $sql->execute();
+
+             if ($res == 0) {
+                  throw new Exception("Falha ao Publicar teste Publicação");
+
+               return false;
+             }
+               return true;
+             
+        }
+         public static function update($recebePost)
+        {
+  
+              $con = Connection::getConn();
+
+              $query = "UPDATE postagem  SET titulo = :tit, conteudo = :cont WHERE id = :id";
+              $sql = $con->prepare($query);
+              $sql->bindValue(":tit", $recebePost['titulo']);
+              $sql->bindValue(":cont", $recebePost['conteudo']);
+              $sql->bindValue(":id", $recebePost['id']);
+                $result = $sql->execute();
+
+             if ( $result == 0) {
+                  throw new Exception("Falha ao Editar Publicação");
+
+               return false;
+             }
+               return true;
+        }
+         public static function delete($recebePost)
+        {
+  
+              $con = Connection::getConn();
+
+              $query = "DELETE FROM postagem  SET titulo = :tit, conteudo = :cont WHERE id = :id";
+              $sql = $con->prepare($query);
+              $sql->bindValue(":tit", $recebePost['titulo']);
+              $sql->bindValue(":cont", $recebePost['conteudo']);
+              $sql->bindValue(":id", $recebePost['id']);
+                $result = $sql->execute();
+
+             if ( $result == 0) {
+                  throw new Exception("Falha ao Editar Publicação");
+
+               return false;
+             }
+               return true;
+             
         }
     }
 
